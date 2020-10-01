418a30d124d4e80deb3250ff1d701ff50854d7fa
Addison-Stavlo <stavz01@gmail.com>
2020-10-01 16:43:53 -0400

    add dependency to selector for selectionEnd

--- packages/block-editor/src/store/selectors.js
+++ packages/block-editor/src/store/selectors.js
@@ -1842,5 +1842,5 @@ export const getActiveBlockIdByBlockNames = createSelector(
 		}
 		return null;
 	},
-	( state ) => [ state.selectionStart.clientId ]
+	( state ) => [ state.selectionStart.clientId, state.selectionEnd.clientId ]
 );
